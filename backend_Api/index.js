c446334fe03fb375de433803753fcc0f2b59a210
odogwukelly <odogwukellyice@gmail.com>
2023-12-21 15:37:43 +0100

    order route completed

--- backend_Api/index.js
+++ backend_Api/index.js
@@ -5,6 +5,9 @@ const dotenv = require("dotenv");
 const userRoute = require("./routes/user");
 const authRoute = require("./routes/auth");
 const productRoute = require("./routes/product");
+const cartRoute = require("./routes/cart");
+const orderRoute = require("./routes/order");
+
 
 dotenv.config();
 
@@ -15,8 +18,10 @@ mongoose.connect(process.env.MONGO_URL)
 
 app.use(express.json());
 app.use("/api/auth", authRoute);
-app.use("/api/user", userRoute);
+app.use("/api/users", userRoute);
 app.use("/api/products", productRoute);
+app.use("/api/carts", cartRoute);
+app.use("/api/orders", orderRoute);
 
 
 
