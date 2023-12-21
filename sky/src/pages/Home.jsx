cb1556b290ac53c5e69baa0bbe63e912865dc57d
ayomide kappo <118881021+ayomidekappo12@users.noreply.github.com>
2023-12-21 06:36:31 +0100

    added new files

--- sky/src/pages/Home.jsx
+++ sky/src/pages/Home.jsx
@@ -1,9 +1,10 @@
 import React from "react";
+import Navbar from "../components/Navbar";
 
 const Home = () => {
   return (
     <div>
-      <h1>Homepage</h1>
+      <Navbar />
     </div>
   );
 };
