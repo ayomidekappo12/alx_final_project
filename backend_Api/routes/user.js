2307bd37de4ca2e557b1d1e274e793ee9e71ad4d
odogwukelly <odogwukellyice@gmail.com>
2023-12-21 10:48:06 +0100

    user CRUD operation

---
+++ backend_Api/routes/user.js
@@ -0,0 +1,21 @@
+const User = require("../models/User"); 
+const {verifyToken, verifyTokenAndAuthorization} = require("./verifyToken");
+const router = require("express").Router();
+
+
+// UPDATE USER
+router.put("/:id", verifyTokenAndAuthorization, async (req, res)=>{
+    if(req.body.password){
+        req.body.password = CryptoJS.AES.encrypt(req.body.password, process.env.PASSWORD_SECRET_KEY).toString();
+    }
+    try {
+        const updatedUser = await User.findByIdAndUpdate(req.params.id, {$set: req.body},{new:true});
+
+        res.status(200).json(updatedUser)
+    }
+    catch(err){
+        res.status(500).json(err);
+    }
+})
+
+module.exports = router;
