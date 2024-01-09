adddf1cc5a48d3ac6f19636d47acacbe20180928
odogwukelly <odogwukellyice@gmail.com>
2024-01-09 17:53:09 +0100

    user, shop and admin configuration done including orders, events, coupon code implementation done

--- Sky_Couture/frontend/src/ShopRoutes.js
+++
@@ -1,7 +0,0 @@
-import ShopHomePage from "./pages/Shop/ShopHomePage";
-
-
-
-export {
-    ShopHomePage,
-}
--- sky/src/responsive.js
+++
@@ -1,7 +0,0 @@
-import { css } from "@emotion/react";
-
-export const mobile = (styles) => css`
-  @media only screen and (max-width: 380px) {
-    ${styles}
-  }
-`;
